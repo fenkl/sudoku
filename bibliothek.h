49070d6f9160f0b2e13e5d8306e5ab4c1b0eac83
Klara-vl <104832779+Klara-vl@users.noreply.github.com>
2022-06-24 11:48:19 +0200

    Update bibliothek.h
    versuch Überprüfung einzufügen

--- bibliothek.h
+++ bibliothek.h
@@ -4,13 +4,22 @@
 #ifndef BIBLIOTHEK_H_INCLUDED
 #define BIBLIOTHEK_H_INCLUDED
 
-//int nullfelder(int array_y, int array_x);
-int IsNumberInArray(int array[9], int number);
-int *check_writable_field (int x, int y, int sudoku[9][9]);
 int Difficulty();
-int *move_cursor();
-int set_cursor(const int x, const int y);
+
+int *sudoku_level(int level);
+
 int level_anzeige (int level_aufbau[9][9]);
-int *sodoku_level(int level);
+
+int *move_cursor();
+
+int set_cursor(const int x, const int y);
+
+int *check_writable_field (int x, int y, int sudoku[9][9]);
+
+int IsNumberInArray(int array[9], int number);
+
+int *Array_ausfuellen_lassen(int bekannte_Zahlen[9][9], int array_zur_Bearbeitung[9][9]);
+
+int Ueberpruefung_der_Loesung(int ausgefuelltes_Sudoku[9][9], int loesung_Sudoku[9][9]);
 
 #endif // BIBLIOTHEK_H_INCLUDED
