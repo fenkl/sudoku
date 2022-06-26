3aa7a86d8571931b86903bab56727729d324f796
HnryHmkr <104819733+HnryHmkr@users.noreply.github.com>
2022-06-26 22:01:50 +0200

    added play again

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <conio.h>
 #include <windows.h>
 
+
 int main()
 {
 
@@ -16,45 +17,60 @@ int main()
     system("cls");                                              // Console leeren
 
     level = sudoku_level_initialisition(choosed_level);                         // = array mit den bekannten Zahlen des Sudokus; Level holen, welches bearbeitet werden soll
+    int exit=0;
+
+
+
+    long start = GetTickCount()/1000.0;
+    do{
+    int zero = anzahl_nullen(level);
+    system("cls");                                              // Console leeren
 
     level_anzeige(level);
+
     printf("\n\n");
     printf("Beenden und Ueberpruefen: ENTER\n");
     printf("Zahl zuruecksetzen:       0");
 
-    long start = GetTickCount()/1000.0;                                 // start Zeitmessung
 
 
     int ausgefuelltes_Sudoku;
+
+
     level = Array_ausfuellen_lassen(level);    //play
 
     system("cls");
 
-    long end = GetTickCount()/1000.0;                                   // Ende Zeitmessung
-    long needed_time = end - start;  // Gesamter Zeitverbrauch
-    long needed_minutes, needed_seconds;
-    needed_seconds = needed_time;
-    // Ausgabe der Zeit
-    if (needed_seconds >= 60) // Zeitumrechnung in Minuten ab 5 Minuten
-    {
-        needed_minutes = needed_time / 60;
-        needed_seconds = needed_time - (needed_minutes * 60);
-        if (needed_minutes == 1)
+
+
+
+    int finish = Ueberpruefung_der_Loesung(level, zero);
+
+    long end = GetTickCount()/1000.0;
+
+    if(finish==0)
         {
-            printf("\n\nBenoetigte Zeit insgesamt: %ld Minute und %ld Sekunden\n", needed_minutes, needed_seconds);
+            printf("\n\n [0]Weiterspielen      [1]Beenden \n");
+            scanf("%i",&exit);
+            if(exit==0)
+            {
+
+                level=weiterspielen(level);
+            }
+            else
+            {
+                zeit_berechnen(start,end);
+            }
         }
         else
-        {
-            printf("\n\nBenoetigte Zeit insgesamt: %ld Minuten und %ld Sekunden\n", needed_minutes, needed_seconds);
-        }
+            {
+            exit = 1;
+            zeit_berechnen(start,end);
+            printf("\n Herzlichen Glueckwunsch, Du hast das Sudoku erfolgreich geloest!");
+            }
     }
-    else // ansonsten ausgabe in Sekunden
-    {
-        printf("\n\nBenoetigte Zeit insgesamt: %ld Sekunden\n", needed_seconds);
-    }
+    while(exit == 0 );
 
-
-    Ueberpruefung_der_Loesung(level);
 
     return 0;
 }
